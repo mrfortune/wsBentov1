dfa20a37dffca06195b63392a7375b7b95516457
mrfortune <rharris@worldshaker.com>
2023-06-28 15:14:10 -0400

    fixes metadata
    Signed-off-by: mrfortune <rharris@worldshaker.com>

--- pages/layout.js
+++ pages/layout.js
@@ -3,10 +3,7 @@ import FooterNav from '../components/FooterNav';
 import AppBar from '../components/AppBar';
 import Meta from '../components/Meta';
 import SimpleBottomNavigation from '../components/bottom';
-export const metadata = {
-  title: 'worldShaker',
-  description: 'Interactive Advertising',
-}
+
 export default function Layout({ children }) {
   return (
     <>
