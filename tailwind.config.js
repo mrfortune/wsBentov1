a3481d4b3f370d069ce851bcee35a1747d4a4486
mrfortune <rharris@worldshaker.com>
2023-08-14 23:37:37 -0400

    nextjs config
    Signed-off-by: mrfortune <rharris@worldshaker.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./styles/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', 
+  './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
