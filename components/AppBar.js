a0d1127fc97e9a1237f79e459b292711082fbb95
mrfortune <rharris@worldshaker.com>
2023-09-26 02:34:40 -0400

    bento boxes begun
    Signed-off-by: mrfortune <rharris@worldshaker.com>

--- components/AppBar.js
+++ components/AppBar.js
@@ -73,9 +73,9 @@ export default function DrawerAppBar (props) {
   const currentRoute = router.pathname;
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box component="nav" sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar component="nav" style={{background:'transparent', boxShadow:'none'}}>
+      <AppBar style={{background:'transparent', boxShadow:'none'}}>
         <Toolbar> 
         <Box sx={{ flexGrow: 1, }}>
 <Typography
@@ -133,13 +133,13 @@ export default function DrawerAppBar (props) {
               }
               else{
                 // eslint-disable-next-line react/jsx-key
-                return ( <Button key={id} size="sm" className="contactButton" color="secondary" css={{ borderRadius: "$xl", marginLeft:'2rem' }}>
+                return ( <button key={id} size="sm" className=" text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-4 mr-2" color="secondary" css={{ borderRadius: "$xl", marginLeft:'2rem' }}>
             <Link href={link.path}
               
             >
             {link.name} 
             </Link>
-          </Button> );
+          </button> );
               }
         })}
        
