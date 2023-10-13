3bbe756175ead759696b05a5be21cb63463bc862
mrfortune <rharris@worldshaker.com>
2023-10-13 08:05:33 -0400

    changes...
    Signed-off-by: mrfortune <rharris@worldshaker.com>

--- pages/layout.js
+++ pages/layout.js
@@ -2,7 +2,7 @@ import DrawerAppBar from '../components/AppBar';
 import FooterNav from '../components/FooterNav';
 import AppBar from '../components/AppBar';
 import Meta from '../components/Meta';
-import { Inter } from 'next/font/google'
+import { Inconsolata } from 'next/font/google'
 import { config, library } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 const inter = Inter({
