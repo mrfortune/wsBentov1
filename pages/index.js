524e506915880c608de7f03bfcb852d2f0324d74
mrfortune <rharris@worldshaker.com>
2023-12-20 15:15:22 -0500

    margin top fix
    Signed-off-by: mrfortune <rharris@worldshaker.com>

--- pages/index.js
+++ pages/index.js
@@ -53,11 +53,47 @@ export default function Home() {
         <title>WorldShaker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className='grid-container'>
+<div>
+      <h1 className="h1Header mb-8">
+                Data, Insights, &
+                Creativity, <br></br>Produce Transformation
+              </h1>
+              <p className="mb-8">
+                At WorldShaker Interactive, we harness the power of data, unearth insights,<br></br> and infuse creativity to catalyze transformative solutions for our clients.
+              </p>
+
+
+              <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium inline-flex rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                <Link className="flex leading-4"
+                  href="/submit-info"
+                >
+
+                  Get Started <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor"  d="M1 5h12m0 0L9 1m4 4L9 9" />
+                  </svg>
+                </Link>
+              </button>
+              <button className="text-white hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900 leading-4">
+                <Link className="leading-4"
+                  href="/contact"
+                >
+                  Learn more...
+                </Link>
+              </button>
+
+      </div>
+      <div></div>
+      <div></div>
+<div></div>
+
+      </div>
+      
       <Box component="section" className="container mx-auto max-w-7xl">
         <Box component="div" className="grid-container"
          
          >
-          <Box sx={{ gridArea: 'header' }} className="mt-40">
+          {/* <Box sx={{ gridArea: 'header' }} className="mt-40">
             <Box
               className="heroText"
               component="div"
@@ -89,7 +125,7 @@ export default function Home() {
                 </Link>
               </button>
             </Box>
-          </Box>
+          </Box> */}
           <Box sx={{ gridArea: 'main' }}>
 
 
