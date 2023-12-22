2a931c3b6abc0bf632cdeda1e459b09d1da9cac2
mrfortune <rharris@worldshaker.com>
2023-12-21 23:27:59 -0500

    bentoBox fixes
    Signed-off-by: mrfortune <rharris@worldshaker.com>

--- app/home.js
+++ app/home.js
@@ -40,39 +40,131 @@ export default function HomePage() {
   return (
 
     <div className="grid-container">
+      <div className="content">
+        <div className="bentoBox rounded-2xl">
+          <h1 className="h1Header mb-8">
+            Data, Insights, &
+            Creativity, <br></br>Produce Transformation
+          </h1>
+          <p className="mb-8">
+            At WorldShaker Interactive, we harness the power of data, unearth insights,<br></br> and infuse creativity to catalyze transformative solutions for our clients.
+          </p>
 
-      <div>
-        <h1 className="h1Header mb-8">
-          Data, Insights, &
-          Creativity, <br></br>Produce Transformation
-        </h1>
-        <p className="mb-8">
-          At WorldShaker Interactive, we harness the power of data, unearth insights,<br></br> and infuse creativity to catalyze transformative solutions for our clients.
-        </p>
 
+          <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium inline-flex rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+            <Link className="flex leading-4"
+              href="/submit-info"
+            >
 
-        <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium inline-flex rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-          <Link className="flex leading-4"
-            href="/submit-info"
-          >
+              Get Started <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" d="M1 5h12m0 0L9 1m4 4L9 9" />
+              </svg>
+            </Link>
+          </button>
+          <button className="text-white hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900 leading-4">
+            <Link className="leading-4"
+              href="/contact"
+            >
+              Learn more...
+            </Link>
+          </button>
 
-            Get Started <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-              <path stroke="currentColor" d="M1 5h12m0 0L9 1m4 4L9 9" />
-            </svg>
-          </Link>
-        </button>
-        <button className="text-white hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900 leading-4">
-          <Link className="leading-4"
-            href="/contact"
-          >
-            Learn more...
-          </Link>
-        </button>
-
+        </div>
       </div>
-      <div></div>
-      <div></div>
-      <div></div>
+
+      <div className="popout">
+        <div className="bentoBox rounded-2xl">
+          <h1 className="h1Header mb-8">
+            Data, Insights, &
+            Creativity, <br></br>Produce Transformation
+          </h1>
+          <p className="mb-8">
+            At WorldShaker Interactive, we harness the power of data, unearth insights,<br></br> and infuse creativity to catalyze transformative solutions for our clients.
+          </p>
+
+
+          <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium inline-flex rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+            <Link className="flex leading-4"
+              href="/submit-info"
+            >
+
+              Get Started <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" d="M1 5h12m0 0L9 1m4 4L9 9" />
+              </svg>
+            </Link>
+          </button>
+          <button className="text-white hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900 leading-4">
+            <Link className="leading-4"
+              href="/contact"
+            >
+              Learn more...
+            </Link>
+          </button>
+
+        </div>
+      </div>
+      <div className="feature">
+        <div className="bentoBox rounded-2xl">
+          <h1 className="h1Header mb-8">
+            Data, Insights, &
+            Creativity, <br></br>Produce Transformation
+          </h1>
+          <p className="mb-8">
+            At WorldShaker Interactive, we harness the power of data, unearth insights,<br></br> and infuse creativity to catalyze transformative solutions for our clients.
+          </p>
+
+
+          <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium inline-flex rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+            <Link className="flex leading-4"
+              href="/submit-info"
+            >
+
+              Get Started <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" d="M1 5h12m0 0L9 1m4 4L9 9" />
+              </svg>
+            </Link>
+          </button>
+          <button className="text-white hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900 leading-4">
+            <Link className="leading-4"
+              href="/contact"
+            >
+              Learn more...
+            </Link>
+          </button>
+
+        </div>
+      </div>
+      <div className="full">
+        <div className="bentoBox rounded-2xl">
+          <h1 className="h1Header mb-8">
+            Data, Insights, &
+            Creativity, <br></br>Produce Transformation
+          </h1>
+          <p className="mb-8">
+            At WorldShaker Interactive, we harness the power of data, unearth insights,<br></br> and infuse creativity to catalyze transformative solutions for our clients.
+          </p>
+
+
+          <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium inline-flex rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+            <Link className="flex leading-4"
+              href="/submit-info"
+            >
+
+              Get Started <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" d="M1 5h12m0 0L9 1m4 4L9 9" />
+              </svg>
+            </Link>
+          </button>
+          <button className="text-white hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900 leading-4">
+            <Link className="leading-4"
+              href="/contact"
+            >
+              Learn more...
+            </Link>
+          </button>
+
+        </div>
+      </div>
 
     </div>
 
