cebf6aa731f58a3a537a01fb67fc2a3d4af917e1
mrfortune <rharris@worldshaker.com>
2024-01-12 10:15:22 -0500

    add services and portfolio...
    Signed-off-by: mrfortune <rharris@worldshaker.com>

--- components/Loader.js
+++
@@ -1,7 +0,0 @@
-import react from 'react';
-
-const myLoader = ({ src, width, quality }) => {
-    return `https://worldshaker.imgix.net/${src}?w=${width}&q=${quality || 75}`
-}
-
-export default myLoader;
